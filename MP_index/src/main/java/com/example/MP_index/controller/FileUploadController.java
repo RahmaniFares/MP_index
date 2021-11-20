aab940ed617e09f774ecfe79f7bee6ad1d9de889
RAHMANI Fares <fares.rahmani@etudiant-isi.utm.tn>
2021-11-20 21:19:34 +0100

    v3

--- MP_index/src/main/java/com/example/MP_index/controller/FileUploadController.java
+++ MP_index/src/main/java/com/example/MP_index/controller/FileUploadController.java
@@ -7,12 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 @Controller
 public class FileUploadController {
@@ -20,8 +23,18 @@ public class FileUploadController {
     private FileUploadService srv;
 
     @PostMapping(value = "/search")
-    public String uploadFile(@RequestParam("img") MultipartFile img) throws IllegalStateException, IOException {
-         boolean res = srv.uploadFile(img,img.getContentType().split("/")[1]);
+    public String uploadFile(@RequestParam("img") MultipartFile img, Model model) throws IllegalStateException, IOException {
+        List<String> l = new LinkedList<>();
+
+        l.add("http://localhost:8080/images/lottie.jpg");
+        l.add("http://localhost:8080/images/lottie.jpg");
+        l.add("http://localhost:8080/images/lottie.jpg");
+        l.add("http://localhost:8080/images/lottie.jpg");
+
+        model.addAttribute("images", l);
+
+
+        //boolean res = srv.uploadFile(img,img.getContentType().split("/")[1]);
         return "Helo";
     }
 
